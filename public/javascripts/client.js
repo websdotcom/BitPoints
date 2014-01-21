25e9ca884854a5699f61c9730b24cd2d0519acab
Nick Swider <swider@webs.com>
2014-01-20 23:26:40 -0500

    Add estimate states, use cards

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,13 +1,28 @@
 var
 	socket = io.connect('http://localhost:3000'),
-	roomId = bp.roomId;
+	roomId = bp.roomId,
+	status = 0, // 0 - start, 1 - betting open, 2 - reveal
+	tim = (function(){var d="{{",a="}}",e="[a-z0-9_][\\.a-z0-9_]*",c=new RegExp(d+"("+e+")"+a,"gim"),b;return function(f,g){return f.replace(c,function(j,l){var n=l.split("."),h=n.length,m=g,k=0;for(;k<h;k++){if(m===b||m===null){break;}m=m[n[k]];if(k===h-1){return m;}}});};}());
+	userTemp = '<li data-user="{{user}}"><img src="{{avatar}}" /><h3>{{user}}</h3><div class="card"><div class="cardBack"></div><div class="cardInner"><div class="cardValue"></div></div></div></li>';
 
 socket.emit("createRoom", {roomId: roomId});
 
 socket.on("newVoter", function(data) {
-	$('<li data-user="'+data.user+'" />').html('<img src="'+data.avatar+'" /> <span class="name">'+data.user+'</span> <span class="points"></span>').appendTo('#users');
+	$(tim(userTemp, data)).appendTo('#users');
 });
 
 socket.on("incomingVote", function(data) {
-	$('li[data-user='+data.user+'] span.points').text(data.estimate);
+	if(status == 1)
+		$('li[data-user='+data.user+'] div.card').addClass('visible').find('div.cardValue').text(data.estimate);
+});
+
+$('#toggleRound').on('click', function(e){
+	status++; if(status == 3){ status = 1; }
+	if(status == 1){ // Start a new round
+		$('h2 span').text('Betting open');
+		$('.card').removeClass('visible showValue');
+	}else if(status == 2){ // Show cards
+		$('h2 span').text('Betting closed');
+		$('.card').addClass('showValue');
+	}
 });

6711884e61352648c8ae2e79d48b1d8563f1ff45
Nick Swider <swider@webs.com>
2014-01-21 18:24:01 -0500

    Have fun with the votes

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -5,15 +5,34 @@ var
 	status = 0, // 0 - start, 1 - betting open, 2 - reveal
 	tim = (function(){var d="{{",a="}}",e="[a-z0-9_][\\.a-z0-9_]*",c=new RegExp(d+"("+e+")"+a,"gim"),b;return function(f,g){return f.replace(c,function(j,l){var n=l.split("."),h=n.length,m=g,k=0;for(;k<h;k++){if(m===b||m===null){break;}m=m[n[k]];if(k===h-1){return m;}}});};}());
 	userTemp = '<li data-user="{{user}}"><img class="voterImage" src="{{avatar}}" /><h3 class="voterName">{{user}}</h3><div class="card"><div class="cardBack"></div><div class="cardInner"><div class="cardValue"></div></div></div></li>',
-	getAverage = function() {
-		var total = 0, numVotes = 0;
+	voteData = {},
+	processVotes = function() {
+		voteData = {
+			average: -1,
+			min: -1,
+			max: -1,
+			allVotesEqual: true,
+			lastVote: -1,
+			total: 0,
+			numVotes: 0
+		};
+		var vote;
 		for(var user in votes) {
 			if(votes.hasOwnProperty(user)) {
-				total += parseInt(votes[user],10);
-				numVotes++;
+				vote = votes[user];
+				voteData.total += vote;
+				if(voteData.lastVote == -1){
+					voteData.lastVote = vote;
+					voteData.min = vote;
+					voteData.max = vote;
+				}
+				if(voteData.lastVote != vote){ voteData.allVotesEqual = false; }
+				if(voteData.max < vote){ voteData.max = vote; }
+				if(voteData.min > vote){ voteData.min = vote; }
+				voteData.numVotes++;
 			}
 		}
-		return numVotes === 0 ? 0 : Math.ceil(total/numVotes);
+		voteData.average = voteData.numVotes === 0 ? 0 : Math.ceil(voteData.total/voteData.numVotes);
 	};
 
 socket.emit("createRoom", {roomId: roomId});
@@ -28,7 +47,7 @@ socket.on("incomingVote", function(data) {
 		$card.find('div.cardValue').text(data.estimate);
 		$card.find('.cardBack').css('background-color', data.color);
 		$card.addClass('visible');
-		votes[data.user] = data.estimate;
+		votes[data.user] = parseInt(data.estimate, 10);
 	}
 });
 
@@ -37,13 +56,26 @@ $('#toggleRound').on('click', function(e){
 	if(status == 1){ // Start a new round
 		$('#average').hide().find('.val').empty();
 		$(this).text('Stop Estimating');
-		$('.card').removeClass('visible showValue');
+		$('.card').removeClass('visible showValue').removeClass('spin');
 		socket.emit("newRound",{roomId: roomId});
 		votes = {};
 	}else if(status == 2){ // Show cards
 		$(this).text('Begin Estimating');
 		$('.card').addClass('showValue');
-		$('#average').show().find('.val').text(getAverage());
+		processVotes();
+		$('#average').show().find('.val').text(voteData.average);
+		if(voteData.numVotes > 3 && voteData.allVotesEqual){
+			$('.card').addClass('spin');
+		}
+		$('.card div.cardValue').each(function(i, el){
+			var
+				$card = $(el),
+				vote = $card.text();
+			if(voteData.min == vote)
+				$card.addClass('min');
+			if(voteData.max == vote)
+				$card.addClass('max');
+		});
 		socket.emit("roundEnd",{roomId: roomId});
 	}
 });

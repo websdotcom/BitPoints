f673430a516dcd05343b3ce3fbc3d77a391c7b48
jackwanders <jack@webs.com>
2014-01-28 10:02:02 -0500

    add validation to join/create forms, fix selection color

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,10 +1,32 @@
+var validateForm = function(form) {
+	var requiredFields = form.find('[data-required=true]');
+	form.removeClass('error');
+
+	requiredFields.each(function() {
+		var field = $(this),
+			val = field.val();
+		if($.trim(val).length < 1) {
+			field.addClass('error');
+			form.addClass('error');
+		} else {
+			field.removeClass('error');
+		}
+	});
+
+	return !form.hasClass('error');
+};
+
 $(function(){
 	$('#join').on('submit', function(e) {
-		document.location = '/roomJoin/' + $('#room-id').val() + '?user=' + $('#user').val() + '&email=' + $('#email').val();
+		if(validateForm($(this))) {
+			document.location = '/roomJoin/' + $('#room-id').val() + '?user=' + $('#user').val() + '&email=' + $('#email').val();
+		}
 		e.preventDefault();
 	});
 	$('#create').on('submit', function(e) {
-		document.location = '/create/?title=' + $('#title').val();
+		if(validateForm($(this))) {
+			document.location = '/create/?title=' + $('#title').val();
+		}
 		e.preventDefault();
 	});
 });
